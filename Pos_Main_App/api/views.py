8348e34f0e3d8b866faea76e0d9f5961be7e8969
AhteshamKhan803 <ahteshamkhangit803@gmail.com>
2025-02-28 22:01:37 +0530

    updated add button added

--- Pos_Main_App/api/views.py
+++ Pos_Main_App/api/views.py
@@ -1,9 +1,17 @@
 from rest_framework.views import APIView
-from Pos_Main_App.models import Dishes_model, Bill_model, OrderedDish_model, Employe_model,Table_model
+from Pos_Main_App.models import Dishes_model, Bill_model,  Employe_model,Table_model
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics, viewsets
+<<<<<<< Updated upstream
 from Pos_Main_App.api.serializers   import Dishes_Serializer, Employe_Serializer, Bill_Serializer, OrderedDish_Serializer, Table_Serializer
 from Pos_Main_App.api.filters import Dishes_filter, OrderedDish_filter,Bill_filter, Employe_filter, Table_filter
+=======
+from Pos_Main_App.api.serializers   import Dishes_Serializer, Employe_Serializer, Bill_Serializer,  Table_Serializer
+from Pos_Main_App.api.filters import Dishes_filter, Bill_filter, Employe_filter, Table_filter
+from django.http import JsonResponse
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+>>>>>>> Stashed changes
 
 
 
@@ -22,9 +30,6 @@ class Dishes_Details_View(generics.RetrieveUpdateDestroyAPIView):
 
 
 
-
-
-
 class Bill_View(generics.ListCreateAPIView):
     queryset = Bill_model.objects.all()
     serializer_class= Bill_Serializer
@@ -36,29 +41,6 @@ class Bill_View(generics.ListCreateAPIView):
 class Bill_Details_View(generics.RetrieveUpdateDestroyAPIView):
     queryset = Bill_model.objects.all()
     serializer_class = Bill_Serializer   
-
-
-
-
-
-
-class OrderDishes_View(generics.ListCreateAPIView):
-    queryset = OrderedDish_model.objects.all()
-    serializer_class= OrderedDish_Serializer
-    filter_backends= [DjangoFilterBackend]
-    filterset_class= OrderedDish_filter
-
-
-
-class orderDishes_Details_View(generics.RetrieveUpdateDestroyAPIView):
-    queryset = OrderedDish_model.objects.all()
-    serializer_class = OrderedDish_Serializer  
-
-
-
-
-
-
 
 
 
@@ -83,7 +65,17 @@ class Table_View(generics.ListCreateAPIView):
     filterset_class= Table_filter
 
 
+class Bill_View(generics.ListCreateAPIView):
+    queryset = Bill_model.objects.all()
+    serializer_class= Bill_Serializer
+    filter_backends= [DjangoFilterBackend]
+    filterset_class=Bill_filter
 
+
+
+class Bill_Details_View(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Bill_model.objects.all()
+    serializer_class = Bill_Serializer   
 
 
 
